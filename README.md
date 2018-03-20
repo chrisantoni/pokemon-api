caa97349903cb4aa34fee27afb181b16e6d7102a
chrisantoni <tonichen184@gmail.com>
2018-03-20 18:14:42 +0700

    add detail

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # pokemon-api
-Simple app to show information about pokemon
+Simple app to show information about pokemon cards
--- src/components/Details.js
+++ src/components/Details.js
@@ -13,19 +13,122 @@ class Details extends Component{
     .then(data => this.setState({pokemon:data.card}))
   }
 
+  handleAbility = () => {
+    let ability = {}
+    for(let item in this.state.pokemon.ability){
+      ability[item] = this.state.pokemon.ability[item]
+    }
+
+    return ability
+  }
+
+  handleAttack = () => {
+    let attacks = []
+    for(let item in this.state.pokemon.attacks){
+      attacks.push({convertedEnergyCost: this.state.pokemon.attacks[item].convertedEnergyCost,
+                    damage : this.state.pokemon.attacks[item].damage,
+                    name:this.state.pokemon.attacks[item].name,
+                    text:this.state.pokemon.attacks[item].text,
+                    cost:this.state.pokemon.attacks[item].cost})
+    }
+
+    return attacks
+
+  }
+
+  handleWeakness = () => {
+    let weakness = []
+    for(let item in this.state.pokemon.weaknesses){
+      weakness.push({type: this.state.pokemon.weaknesses[item].type,
+                    value : this.state.pokemon.weaknesses[item].value
+                  })
+    }
+    return weakness
+  }
+
   render(){
-    const { imageUrl,name } = this.state.pokemon
+    const { imageUrl,name,artist,hp,rarity,series} = this.state.pokemon
+    console.log("pokemon ",this.state.pokemon)
+    const ability = this.handleAbility()
+    const attacks = this.handleAttack()
+    const weakness = this.handleWeakness()
+    console.log("weakness ",weakness)
     return (
       <div className = "container">
         <Header page = "details"/>
-        <div className="col-sm-6 col-md-4">
+      <div className ="row">
+        <div className="col-md-6">
           <div className="thumbnail">
+            <h1 className = "text-center">{name}</h1>
             <img src = {imageUrl } alt="img"></img>
-            <div className="caption">
-              <h3>{ name }</h3>
-            </div>
           </div>
         </div>
+        <div className="col-md-6">
+          <p>Name : {name}</p>
+          <p>Artist : {artist}</p>
+          <p>Hp : {hp}</p>
+          <p>Rarity : {rarity}</p>
+          <p>Series : {series}</p>
+            {
+              this.state.pokemon.ability &&
+            <p>Ability :
+              <ul>
+                  <li>name : {ability.name}</li>
+                  <li>text : {ability.text}</li>
+                  <li>type : {ability.type}</li>
+
+              </ul>
+            </p>
+            }
+
+            {
+              this.state.pokemon.attacks &&
+            <p>Attacks :
+                {
+                  attacks.map((attack,idx) => {
+                  return (
+                    <ul>[ {idx} ]
+                    <li>convertedEnergyCost : {attack.convertedEnergyCost}</li>
+                    <li>damage : {attack.damage}</li>
+                    <li>name : {attack.name}</li>
+                    <li>text : {attack.text}</li>
+                    cost : {
+                      attack.cost.map(cost => {
+                        return (
+                          <li style={{marginLeft:40,listStyleType:"circle"}}> {cost} </li>
+                        )
+                      })
+                      }
+                    </ul>
+                  )
+                })
+              }
+
+            </p>
+            }
+
+            {
+              this.state.pokemon.weaknesses &&
+            <p>Weaknesses :
+                {
+                  weakness.map((weakness,idx) => {
+                  return (
+                    <ul>[ {idx} ]
+                    <li>type : {weakness.type}</li>
+                    <li>value : {weakness.value}</li>
+                
+                    </ul>
+                  )
+                })
+              }
+
+            </p>
+            }
+
+
+
+        </div>
+      </div>
       </div>
     )
   }
